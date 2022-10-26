fa6c4e73fb9371f3052a0fcb5fb9cb9b327ea9ed
root <root@host01.local>
2022-10-25 22:26:10 -0300

    clarify verify

--- verify.sh
+++ verify.sh
@@ -10,14 +10,30 @@ fi
 export name
 . ./paths.ini
 
+echo 'root key'
 openssl rsa -noout -text  -in ${root_key}
+echo ''
+echo ''
+echo 'root csr'
 openssl req -noout -text  -in ${root_csr}
+echo ''
+echo ''
+echo 'root crt'
 openssl x509 -text -noout -in ${root_crt}
+echo ''
+echo ''
 
 if [[ $flag -eq 1 ]]
 then
+    echo 'root key'
     openssl rsa -noout -text  -in $key
+    echo ''
+    echo ''
+    echo 'this csr'
     openssl req -noout -text  -in $csr
+    echo ''
+    echo ''
+    echo 'this crt'
     openssl x509 -noout -text -in $crt
 fi
 #openssl pkcs12 -export -inkey $key -in $crt -certfile ${root_crt} -out /tmp/${name}.p12
