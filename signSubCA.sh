370b2a27780bbd1bff5e0690deddde64ebc0df2b
savvyaxl <102387781+savvyaxl@users.noreply.github.com>
2024-08-04 14:17:28 -0300

    Update signSubCA.sh

--- signSubCA.sh
+++ signSubCA.sh
@@ -19,8 +19,7 @@ if [[ ! -f ${DB} ]]; then touch ${DB}; fi
 if [[ ! -f ${index} ]]; then echo ${date_}0001 > ${index}; fi
 
 
-#openssl ca -batch -in ${csr} -out ${crt} -keyfile ${root_key} -cert ${root_crt} -config <(
-openssl ca -gencrl -keyfile ${root_key} -cert ${root_crt} -out ${root_crl} -config <(
+openssl ca -batch -in ${csr} -out ${crt} -keyfile ${root_key} -cert ${root_crt} -config <(
 cat <<-EOF
 [ default ]
 ca                      = root-ca               # CA name
